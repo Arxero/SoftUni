767df5602b0a480388f7601c1cbd30e96873507b
Arxero <30701352+Arxero@users.noreply.github.com>
2017-08-03 21:52:40 +0300

    test
    test

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25.Parichna_Nagrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int part = int.Parse(Console.ReadLine()); // 6:47
+            double money = double.Parse(Console.ReadLine());
+
+            double pointsOdd = 0;
+            double pointsEven = 0;
+            double sumAllPoints = 0;
+
+            for (int i = 0; i < part; i++)
+            {
+                int points = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    // odd
+                    pointsOdd += points;
+                }
+                else
+                {
+                    //even
+                    pointsEven += points * 2;
+                }
+            }
+            sumAllPoints = pointsOdd + pointsEven;
+            Console.WriteLine($"The project prize was {money * sumAllPoints:f2} lv.");
+        }
+    }
+}
